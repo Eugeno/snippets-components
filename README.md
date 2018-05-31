39be33f3891deca95bf8849cd1d6da91634ba84b
eugeno <eugeneath@yandex.ru>
2018-05-31 14:49:45 +0700

    add 'tabs'

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-Collection of useful frontend code snippets. Collect for myself, but it can be useful for others.
+Collection of useful frontend code snippets and components. Collect for myself, but it can be useful for others.
 
 [Calculating the scrollbar width](snippets/scrollbar-width)<br>
-[Work with potentially non-existent data in object](snippets/potentially-non-existent-data)
+[Work with potentially non-existent data in object](snippets/potentially-non-existent-data)<br>
+[Tabs](snippets/tabs)
---
+++ tabs/src/js/tabs.js
@@ -0,0 +1,55 @@
+class Tabs {
+  constructor (options) {
+    const defaults = {
+      activeClass: '_is-active',
+      hiddenClass: '_is-hidden',
+    }
+
+    this.settings = Object.assign({}, defaults, options)
+
+    this.init()
+  }
+
+  init () {
+    const tabsContainers = [...document.querySelectorAll('[data-tabs]')]
+    tabsContainers.forEach((tabsContainer, i) => {
+      const tabs = [...tabsContainer.querySelectorAll('[data-tab]')]
+      const tabsContent = tabsContainer.dataset.tab ?
+        document.querySelector(`[data-tabs-content=${tabsContainer.dataset.tab}]`) :
+        [...document.querySelectorAll('[data-tabs-content]')][i]
+
+      const tabsMap = new Map()
+      tabs.forEach((tab, j) => {
+        const tabContent = tab.dataset.tab ?
+          tabsContent.querySelector(`[data-tab=${tab.dataset.tab}]`) :
+          [...tabsContent.querySelectorAll('[data-tab]')][j]
+        tabsMap.set(tab, tabContent)
+      })
+
+      tabsContainer.addEventListener('click', (event) => {
+        let target = event.target
+        while (!target.hasAttribute('data-tab')) {
+          target = target.parentNode
+        }
+
+        this.switch(target, tabsMap)
+      })
+
+      if (!tabsContainer.querySelector(`[data-tab].${this.settings.activeClass}`)) {
+        this.switch(tabs[0], tabsMap)
+      }
+    })
+  }
+
+  switch (target, tabsMap) {
+    for (let [tab, tabsContent] of tabsMap) {
+      tab.classList.remove(this.settings.hiddenClass)
+      tabsContent.classList.add(this.settings.hiddenClass)
+    }
+
+    target.classList.add(this.settings.activeClass)
+    tabsMap.get(target).classList.remove(this.settings.hiddenClass)
+  }
+}
+
+export default Tabs
